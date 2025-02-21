3160d35670615e370a6f19521232c74fa4787aa6
nadineorriss <165856992+nadineorriss@users.noreply.github.com>
2025-02-21 08:56:39 -0800

    step 1.3

---
+++ map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm9ycnJpc3MiLCJhIjoiY203ZjAxYzR0MGs0ZjJycHFqcGN4bmllaCJ9.HoBz0Fn22Gi5wH-vWRyoWA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],  // Boston coordinates
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18
+});
